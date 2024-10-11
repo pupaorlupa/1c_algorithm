1f7f61c57d4e52a960c2feaa875c6174d9c50e0b
Дмитрий Беликов <pupaorlupa@pupaorlupa-Mac.local>
2024-10-11 23:11:54 +0300

    version 1

---
+++ main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <string_view>
+
+#include "trie.cpp"
+
+std::vector<std::string_view> SplitWords(std::string_view text) {
+    std::vector<std::string_view> words;
+    std::size_t start = 0;
+    
+    while (start < text.size()) {
+        std::size_t end = text.find(' ', start);
+        
+        if (end == std::string_view::npos) {
+            words.emplace_back(text.substr(start));
+            break;
+        } else {
+            if (end != start) {
+                words.emplace_back(text.substr(start, end - start));
+            }
+        }
+        
+        start = end + 1;
+    }
+
+    return words;
+}
+
+int main() {
+    Trie trie;
+
+    std::string command;
+    std::string text;
+    while (true) {
+        std::cin >> command;
+        if (command == "add") {
+            std::cin.ignore();
+            std::getline(std::cin, text);
+
+            for (auto word : SplitWords(text)) {
+                trie.Add(word);
+            }
+            std::cout << "[ANS]Added successfully" << std::endl;
+        } else if (command == "predict") {
+            bool is_new = false;
+            std::cin.ignore();
+            std::getline(std::cin, text);
+
+            if (text.find(" ") != std::string::npos) {
+                is_new = true;
+            }
+            std::cout << "[ANS]Suggested: ";
+            std::cout << trie.Predict(*(SplitWords(text).end() - 1), is_new) << std::endl;
+        } else if (command == "exit") {
+            std::cout << "[ANS]Программа успешно завершилась." << std::endl;
+            return 0;
+        } else {
+            std::cout << "[ANS]Неверный формат запроса" << std::endl;
+            std::cout << "[ANS]Была получена комманда " << command << std::endl;
+        }
+    }
+}
---
+++ trie.cpp
@@ -0,0 +1,107 @@
+#include <unordered_map>
+#include <vector>
+
+struct Trie {
+private:
+  struct Vertex {
+    std::unordered_map<char, int> to;
+    int terminal_score;
+    int best_next_index;
+    int best_next_score;
+    char letter;
+
+    Vertex(int index, char letter) : terminal_score(0), best_next_index(index), best_next_score(0), letter(letter) {}
+  };
+
+public:
+  struct TrieState {
+    std::string prefix;
+    
+    int vertex_index = 0;
+
+    void Clear() {
+      vertex_index = 0;
+      prefix.clear();
+    }
+  };
+
+  Trie() {
+    trie.push_back(Vertex(0, '\0'));
+  }
+
+  /// Добавляет вершину в бор и пересчитывает максимумы предков в каждой вершине на пути.
+  void Add(const std::string_view word) {
+    state.Clear();
+
+    std::vector<int> history; // будет явно запоминать историю использованных вершин для пересчета максимума, потому что рекурсия это плохо
+                              // TODO: использовать StackVector длины ~6, потому что как правило слова не длинные
+    history.reserve(word.size() + 1);
+
+    int current_vertex = 0;
+    for (size_t i = 0; i < word.size(); ++i) {
+      history.push_back(current_vertex);
+
+      auto place = trie[current_vertex].to.try_emplace(word[i]);
+      if (place.second) {
+        (*place.first).second = trie.size();
+        trie.push_back(Vertex(trie.size(), word[i]));
+      }
+      current_vertex = (*(place.first)).second;
+    }
+
+    history.push_back(current_vertex);
+    auto& vert = trie[current_vertex];
+    ++vert.terminal_score;
+    if (vert.best_next_score < vert.terminal_score) {
+      vert.best_next_index = current_vertex;
+      vert.best_next_score = vert.terminal_score;
+    }
+    
+    // выполним обновление максимумов
+    for (int i = history.size() - 2; i >= 0; --i) {
+      int current_vertex_index = history[i];
+      auto& vert = trie[current_vertex_index];
+
+      int next_score = trie[history[i + 1]].best_next_score;
+      if (vert.best_next_score < next_score) {
+        vert.best_next_score = next_score;
+        vert.best_next_index = history[i + 1];
+      } else {
+        break;
+      }
+    }
+  }
+
+  std::string Predict(std::string_view word, bool is_new) {
+    if (is_new) {
+      state.Clear();
+    }
+
+    int current_vertex_index = state.vertex_index;
+    for (auto letter : word) {
+      auto& vert = trie[current_vertex_index];
+      auto next_vert = vert.to.find(letter);
+      if (next_vert != vert.to.end()) {
+        state.prefix += letter; 
+        current_vertex_index = (*next_vert).second;
+        state.vertex_index = current_vertex_index;
+      } else {
+        state.Clear();
+        return "NO PREDICTIONS";
+      }
+    }
+
+    std::string ret_prefix = state.prefix;
+
+    while (current_vertex_index != trie[current_vertex_index].best_next_index) {
+      current_vertex_index = trie[current_vertex_index].best_next_index;
+      ret_prefix += trie[current_vertex_index].letter;
+    }
+    return ret_prefix;
+  }
+
+private:
+
+  TrieState state;
+  std::vector<Vertex> trie;
+};
